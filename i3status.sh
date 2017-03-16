01ffc18f00ead9e5aab8e03b10cdbd3d112d3692
Ian Hunter <beanland2@gmail.com>
2017-03-16 09:41:31 -0400

    Removed buggy pomodoro timer.

--- i3status.sh
+++ i3status.sh
@@ -2,6 +2,5 @@
 i3status -c ~/.config/i3status/config | while :
 do
   read line
-  pomodoro=`python ~/i3-gnome-pomodoro/pomodoro-client.py status`
-  echo "$pomodoro| $line" || exit 1
+  echo "$line" || exit 1
 done
